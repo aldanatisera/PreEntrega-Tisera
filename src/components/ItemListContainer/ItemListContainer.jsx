16a7967506b7f435b0c4c966b359f99c38ba720d
aldana tisera <aldanatisera99@gmail.com>
2024-01-23 18:54:49 -0300

    Change text

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,15 +1,17 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import ItemCounter from "../ItemCounter/ItemCounter";
+
+const handleAdd = () => {};
+
 function ItemListContainer({ title, stock }) {
   return (
     <>
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
-          <Card.Title>Motorola</Card.Title>
-          <Card.Text>Motorola</Card.Text>
-          <Button variant="primary">Add cart</Button>
+          <Card.Title>Uñas 1</Card.Title>
+          <Card.Text>Este es el primer modelo de uña</Card.Text>
           <ItemCounter stock={stock} />
         </Card.Body>
       </Card>
